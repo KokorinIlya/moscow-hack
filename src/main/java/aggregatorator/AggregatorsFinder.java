f2c7e7fa96d97d14afd4586da44985a3c9f3a99e
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2018-12-09 20:54:17 +0300

    a

--- src/main/java/aggregatorator/AggregatorsFinder.java
+++ src/main/java/aggregatorator/AggregatorsFinder.java
@@ -1,31 +1,31 @@
-package aggregatorator;
-
-import components.Site;
-import components.SiteCounter;
-import lombok.NoArgsConstructor;
-
-import java.util.HashMap;
-
-@NoArgsConstructor
-public class AggregatorsFinder {
-	public static long MIN_AGGREGATOR_REPETITION_NUMBER = 3;
-
-	HashMap<String, SiteCounter> data;
-
-	public void addSite(Site site) {
-		SiteCounter siteCounter = data.get(site.getUrl());
-		if (siteCounter != null) {
-			siteCounter.incrementNumber();
-			if (isAggregator(siteCounter)) {
-				// TODO: add to DB
-				data.remove(site.getUrl());
-			}
-		} else {
-			data.put(site.getUrl(), new SiteCounter(site, 1));
-		}
-	}
-
-	private boolean isAggregator(SiteCounter siteCounter) {
-		return siteCounter.getNumber() >= MIN_AGGREGATOR_REPETITION_NUMBER;
-	}
-}
+//package aggregatorator;
+//
+//import components.Site;
+//import components.SiteCounter;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.HashMap;
+//
+//@NoArgsConstructor
+//public class AggregatorsFinder {
+//	public static long MIN_AGGREGATOR_REPETITION_NUMBER = 3;
+//
+//	HashMap<String, SiteCounter> data;
+//
+//	public void addSite(Site site) {
+//		SiteCounter siteCounter = data.get(site.getUrl());
+//		if (siteCounter != null) {
+//			siteCounter.incrementNumber();
+//			if (isAggregator(siteCounter)) {
+//				// TODO: add to DB
+//				data.remove(site.getUrl());
+//			}
+//		} else {
+//			data.put(site.getUrl(), new SiteCounter(site, 1));
+//		}
+//	}
+//
+//	private boolean isAggregator(SiteCounter siteCounter) {
+//		return siteCounter.getNumber() >= MIN_AGGREGATOR_REPETITION_NUMBER;
+//	}
+//}
---
+++ src/main/scala/TagsByCompanyCollector.scala
@@ -0,0 +1,5 @@
+object TagsByCompanyCollector {
+  def getTags(companyName: String) = {
+    val sites = 
+  }
+}
