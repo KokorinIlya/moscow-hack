40c3671ce7b51afd34915fc9574985c5c50811db
macbook <kir-esakov2011@mail.ru>
2018-12-09 11:59:01 +0300

    added db with spring

---
+++ src/main/java/db/entity/Companies.java
@@ -0,0 +1,64 @@
+package db.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Entity
+@Table(name = "companies", schema = "public")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@ToString(of = {"id", "author", "title", "creationDate", "alteringDate", "content", "contentPreview"})
+@Getter
+@Setter
+public class Companies implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "news_id_seq")
+	@SequenceGenerator(name = "news_id_seq", sequenceName = "news_id_seq", allocationSize = 1)
+	@Column(name = "id")
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "id_user", nullable = false)
+	@JsonManagedReference
+	private User author;
+
+	@Column(name = "title", nullable = false)
+	private String title;
+
+	@Column(name = "creation_date", nullable = false)
+	private Timestamp creationDate;
+
+	@Column(name = "altering_date")
+	private Timestamp alteringDate;
+
+	@Column(name = "content", nullable = false)
+	private String content;
+
+	@Column(name = "content_preview", nullable = false)
+	private String contentPreview;
+
+	@OneToMany(mappedBy="news", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval=true)
+	@JsonBackReference
+//	@Column(nullable = false) // TODO: nullable for columns, fetch type, cascade
+	private Set<Comment> comments;
+
+	@OneToMany(mappedBy="news", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval=true)
+	@JsonBackReference
+	private Set<NewsLoop> newsLoops;
+
+	@OneToMany(mappedBy="news", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval=true)
+	@JsonBackReference
+	private Set<NewsPoop> newsPoops;
+
+//	@OneToMany(mappedBy="news", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval=true)
+//	@JsonBackReference
+//	private Set<Repost> reposts;
+}
---
+++ src/main/java/db/repository/CompaniesRepo.java
@@ -0,0 +1,49 @@
+package db.repository;
+
+
+import db.entity.Companies;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+//@RepositoryRestResource(collectionResourceRel = "news", path = "news", excerptProjection = NewsForFeedProjection.class)
+@Repository
+public interface CompaniesRepo extends JpaRepository<Companies, Long> {
+	//@RestResource(exported = false)
+	@Query("select n from News n, Subscription s where n.author = s.onWhom and s.who.id = :userId")
+	List<Companies> getAllForUserId(@Param("userId") long userId, Pageable pagination);
+
+	//@RestResource(exported = false)
+	@Query("select n from News n where n.author.id = :userId")
+	List<Companies> getAllByUserId(@Param("userId") long userId, Pageable pagination);
+
+	//@RestResource(exported = false)
+	@Query("select n from News n where n.id = :newsId")
+	Companies getOneByNewsId(@Param("newsId") long newsId);
+
+	void removeById(Long id);
+
+//	@Query(value = "SELECT u.id AS id, u.login AS authorLogin, n.content_preview AS contentPreview, " +
+//			"count(lop) AS loopsNumber, count(pop) AS poopsNumber, count(com) AS commentsNumber, " +
+//			"count(rep) AS repostsNumber, n.creation_date AS creationDate, n.altering_date AS " +
+//			"alteringDate " +
+//			"FROM public.news n, public.news_loop lop, public.news_poop pop, public.comment com , " +
+//			"public.repost rep, public.user u " +
+//			"WHERE u.id = n.id_user AND lop.id_news = n.id AND pop.id_news = n.id AND " +
+//			"com.id_news = n.id AND n.id IN ( " +
+//			"SELECT rep.id_news " +
+//			"FROM public.repost rep, public.subscription sub " +
+//			"WHERE rep.id_user = sub.id_on_whom " +
+//			")" +
+//			"GROUP BY u.id, u.login, n.content_preview, n.creation_date, n.altering_date " +
+//			"ORDER BY n.altering_date", nativeQuery = true)
+//	List<Object[]> findAllNewsPreviews();
+
+	//@Query("select u from User u where u.firstname = :firstname or u.lastname = :lastname")
+	//  User findByLastnameOrFirstname(@Param("lastname") String lastname,
+	//                                 @Param("firstname") String firstname);
+}
